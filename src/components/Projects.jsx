3681a1938996a3a134b4cb4f77b29cc9502a3727
Saqibi4213 <mehriamosawer@gmail.com>
2025-01-11 19:17:59 +0430

    solved

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,6 +12,7 @@ const projects = [
     name: 'Phone Validator',
     description: 'A responsive phone validator showcasing my work and skills.',
     image: '/images/phone-validator.png',
+    /* eslint-disable react/jsx-key */
     technologies: [<FaHtml5 />, <FaCss3Alt />, <FaJs />],
   },
   {
@@ -19,25 +20,29 @@ const projects = [
     name: 'Skin Care Website',
     description: 'A skin care website showcasing my work and skills.',
     image: '/images/skin-care.png',
+    /* eslint-disable react/jsx-key */
     technologies: [<FaHtml5 />, <FaCss3Alt />],
   },
   {
     id: 3,
     name: 'Pokemon',
     description: 'A website named Pokemon showcasing all Pokemon characters.',
+    /* eslint-disable react/jsx-key */
     image: '/images/pokemon.png',
     technologies: [<FaHtml5 />, <FaCss3Alt />, <FaJs />],
   },
   {
     id: 4,
     name: 'Travels Space',
+    /* eslint-disable react/jsx-key */
     description: 'A project using React JS for a travels space app.',
-    image: '/images/travels.png',
+    image: '/images/travels.png.png',
     technologies: [<FaReact />, <FaJs />],
   },
   {
     id: 5,
     name: 'Cash Register',
+    /* eslint-disable react/jsx-key */
     description: 'A cash register app helping to return change to customers.',
     image: '/images/C1.png',
     technologies: [<FaHtml5 />, <FaCss3Alt />, <FaJs />],
@@ -45,9 +50,10 @@ const projects = [
   {
     id: 6,
     name: 'Next.js Advanced App',
+    /* eslint-disable react/jsx-key */
     description: 'A Next.js app showcasing advanced skills and practices.',
-    image: '/images/nextjs.png',
-    technologies: [<FaHtml5 />, <FaCss3Alt />],
+    image: '/images/nextjs.png.png',
+    technologies: [<FaReact />, <FaJs />],
   },
 ];
 
@@ -70,6 +76,7 @@ const Projects = () => (
               <h3 className="text-xl font-semibold mb-2">{project.name}</h3>
               <p className="text-sm mb-4">{project.description}</p>
               <div className="flex space-x-2">
+              /* eslint-disable react/jsx-key */
                 {project.technologies.map((icon, index) => (
                   <span
                     key={`${project.id}-${index}`} // Unique key for each technology
